82c29a76cd4c8717896bee1f844b73faa91434ad
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-06-01 13:15:04 +0200

    Add project files.

---
+++ MvcAWSElastiCache/Controllers/CochesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAWSElastiCache.Models;
+using MvcAWSElastiCache.Repositories;
+using MvcAWSElastiCache.Services;
+
+namespace MvcAWSElastiCache.Controllers
+{
+    public class CochesController : Controller
+    {
+        private RepositoryCoches repo;
+        private ServiceAWSCache service;
+
+        public CochesController(RepositoryCoches repo, ServiceAWSCache service)
+        {
+            this.repo = repo;
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            List<Coche> cars = this.repo.GetCoches();
+            return View(cars);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Coche car = this.repo.FindCoche(id);
+            return View(car);
+        }
+
+        public async Task<IActionResult> Favoritos(int id)
+        {
+            List<Coche> coches = await this.service.GetCochesAsync();
+            return View(coches);
+        }
+
+        public async Task<IActionResult> SeleccionarFavorito(int id)
+        {
+            Coche favorito = this.repo.FindCoche(id);
+            await this.service.AddFavoritoAsync(favorito);
+            return RedirectToAction("Favoritos");
+        }
+
+        public async Task<IActionResult> DeleteFavorito(int id)
+        {
+            await this.service.DeleteFavoritoAsync(id);
+            return RedirectToAction("Favoritos");
+        }
+    }
+}
---
+++ MvcAWSElastiCache/Helpers/HelperCache.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+namespace MvcAWSElastiCache.Helpers
+{
+    public class HelperCache
+    {
+        private static Lazy<ConnectionMultiplexer>
+            CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                string connectionString = @"cache-coches.tltuaq.ng.0001.use1.cache.amazonaws.com:6379";
+                return ConnectionMultiplexer.Connect(connectionString);
+            });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
